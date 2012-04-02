5ec709a5d4955c7392905c4e8ac21953d46f0c52
James Reeves <jreeves@weavejester.com>
2012-04-02 17:24:36 +0100

    Added json-response function

--- src/ring/util/json.clj
+++ src/ring/util/json.clj
@@ -1,1 +1,9 @@
-(ns ring.util.json)
+(ns ring.util.json
+  (:require [cheshire.core :as json])
+  (:use [ring.util.response :only (response content-type)]))
+
+(defn json-response
+  "Turn the supplied data into a JSON-encoded Ring response."
+  [data]
+  (-> (response (json/generate-string data))
+      (content-type "application/json")))
--- test/ring/util/test/json.clj
+++ test/ring/util/test/json.clj
@@ -2,5 +2,7 @@
   (:use ring.util.json
         clojure.test))
 
-(deftest replace-me
-  (is false))
+(deftest json-response-test
+  (let [resp (json-response {:foo "bar"})]
+    (is (= (get-in resp [:headers "Content-Type"]) "application/json"))
+    (is (= (resp :body) "{\"foo\":\"bar\"}"))))
