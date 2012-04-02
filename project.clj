41bfdfff37f9a64c8708b753a9f14a13786db04e
James Reeves <jreeves@weavejester.com>
2012-04-02 17:13:16 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ring-json-response "0.1.0-SNAPSHOT"
+  :description "Ring responses in JSON"
+  :url "https://github.com/weavejester/ring-json-response"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [ring/ring-core "1.0.2"]
+                 [cheshire "3.1.0"]])
---
+++ test/ring/util/test/json.clj
@@ -0,0 +1,6 @@
+(ns ring.util.test.json
+  (:use ring.util.json
+        clojure.test))
+
+(deftest replace-me
+  (is false))
